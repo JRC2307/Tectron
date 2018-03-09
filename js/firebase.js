180be6314fcdd0ba5e5584c07124810eee94e9bc
Franco Valencia <franco.avalencia@gmail.com>
2018-03-08 18:40:52 -0600

    Work in index

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBNqb5PRq4jNW6gHvxx2j8E1T8-FAnXGYE",
+  authDomain: "tectron111.firebaseapp.com",
+  databaseURL: "https://tectron111.firebaseio.com",
+  projectId: "tectron111",
+  storageBucket: "tectron111.appspot.com",
+  messagingSenderId: "1087328543626"
+};
+
+firebase.initializeApp(config);
